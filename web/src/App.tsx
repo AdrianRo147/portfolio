d5b6e26e23a768a09b1e636e3db9530ee4b3a119
AdrianRo147 <adrian.rovnan@gmail.com>
2025-08-29 01:44:55 +0200

    updated entry point

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,6 +1,7 @@
 import AboutUI from "./ui/components/about"
 import CertificationsUI from "./ui/components/certifications"
 import ContactUI from "./ui/components/contact-form"
+import EducationUI from "./ui/components/education"
 import HeroUI from "./ui/components/hero"
 import NavbarUI from "./ui/components/navbar"
 import ProjectsUI from "./ui/components/projects"
@@ -14,6 +15,7 @@ function App() {
       <HeroUI />
       <AboutUI />
       <SkillsUI />
+      <EducationUI />
       <CertificationsUI />
       <ProjectsUI />
       <ContactUI />
