f4be964dd37f4196613014ebbc6addb6ecf7ea9a
AdrianRo147 <adrian.rovnan@gmail.com>
2025-08-29 21:19:08 +0200

    use ip whitelist using express custom middleware instead of cors

--- server/index.js
+++ server/index.js
@@ -1,14 +1,12 @@
 const express = require('express');
-const cors = require('cors');
 const app = express();
 const port = 3000;
-
-app.use(cors());
 
 const allowedIps = ['192.168.100.5', '127.0.0.1'];
 
 app.use((req, res, next) => {
-  const clientIp = req.ip.replace('::ffff:', ''); // normalize IPv4-mapped IPv6
+  const clientIp = req.ip.replace('::ffff:', '');
+
   if (allowedIps.includes(clientIp)) {
     next();
   } else {
